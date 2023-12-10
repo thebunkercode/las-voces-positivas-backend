241cbc95591879da0bc830146f7ea444d82f218b
lmacedo-belity <falconshady@gmail.com>
2023-12-09 23:14:53 -0500

    users login api 100%

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Users\UsersCreateRequest;
+use App\Http\Requests\Users\UsersLoginRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Users extends Controller
 {
@@ -16,6 +18,20 @@ class Users extends Controller
         if ($create) {
             $response['success'] = true;
             $response['response'] = $create;
+        }
+        return response()->json($response);
+    }
+
+    public function login(UsersLoginRequest $request)
+    {
+        $response = [
+            'success' => false,
+        ];
+        $data = $request->all();
+        $user = User::where('username', $data['username'])->first();
+        if (!empty($user) && Hash::check($data['password'], $user->password)) {
+            $response['success'] = true;
+            $response['response'] = $user;
         }
         return response()->json($response);
     }
