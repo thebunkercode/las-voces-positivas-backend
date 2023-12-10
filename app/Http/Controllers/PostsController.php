f1d6555eb96cfdc2a6179151b9a6f9c8a26fb301
lmacedo-belity <falconshady@gmail.com>
2023-12-10 03:37:22 -0500

    add post pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,14 +8,27 @@ use Illuminate\Http\Request;
 class PostsController extends Controller
 {
     public function list(Request $request){
+        $limit = 5;
+        $offset = 5;
+        $page = 1;
+        if($request->has('page')){
+            $page = $request->get('page');
+        }
         if($request->has('with')){
-            $posts = Post::with('user')->with('comments.user')->get();
+            $posts = Post::with('user')->with('comments.user')
+                ->offset($offset*($page-1))
+                ->limit($limit)
+                ->get();
         }else{
-            $posts = Post::with('user')->get();
+            $posts = Post::with('user')
+                ->offset($offset*($page-1))
+                ->limit($limit)
+                ->get();
         }
         return response()->json([
             'success' => true,
-            'rows' => $posts 
+            'rows' => $posts,
+            'total' => count($posts)
         ]);
     }
 }
